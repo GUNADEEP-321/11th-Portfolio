ffb95e17b2b725b882f12230eb71196650276716
Gunadeep <puligundlagunadeep321@gmail.com>
2026-06-02 18:47:01 +0530

    465 smtp test

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ app.use(express.static(__dirname));
 const transporter = nodemailer.createTransport({
 
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+
+  port: 465,
+
+  secure: true,
 
   auth: {
     user: process.env.EMAIL_USER,
